69741948308e3a5101b83b03d3722f93a4f06298
Betrayer <StarDestroyer@ukr.net>
2020-03-13 09:49:58 +0200

    Updates

---
+++ precache-manifest.6073385d77b0d8e394134cf02184f537.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63bcb6817d55f466e40812fcaafa4cc",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "bafe8be2a5437275307e",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a51cb528.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a51cb528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a81385c068c38a0dbd9",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.56f27886.chunk.js"
+  },
+  {
+    "revision": "238a1528aed3a14ce705",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d660e622.js"
+  }
+]);
