3f4b22088fc5cca5b4a719c79c0f456265c4c828
Betrayer <StarDestroyer@ukr.net>
2020-03-13 09:48:45 +0200

    hw_done

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,36 @@ import Filter from "./filter/Filter";
 class App extends Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" }
+      // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      // { id: "id-4", name: "Annie Copeland", number: "227-91-26" }
     ],
     filter: ""
   };
 
+  componentDidMount() {
+    const contacts =
+      localStorage.getItem("contacts") !== null
+        ? JSON.parse(localStorage.getItem("contacts"))
+        : [];
+    this.setState({ contacts });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+  }
+
   submitContact = data => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, data]
-    }));
+    const isNameExist = this.state.contacts.some(
+      contact => contact.name === data.name
+    );
+    console.log("isNameExist", isNameExist);
+    !isNameExist
+      ? this.setState(prevState => ({
+          contacts: [...prevState.contacts, data]
+        }))
+      : alert("Name already taken!");
   };
 
   deleteContact = e => {
@@ -46,7 +64,9 @@ class App extends Component {
         <ContactForm submitContact={this.submitContact} />
 
         <h2>Contacts</h2>
-        <Filter nameFilter={this.nameFilter} />
+        {this.state.contacts.length > 2 && (
+          <Filter nameFilter={this.nameFilter} />
+        )}
         <ContactList
           contacts={this.getFilteredContacts()}
           deleteContact={this.deleteContact}
